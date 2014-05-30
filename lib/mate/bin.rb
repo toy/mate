299d4198708129b5b8bfc69736e5afa65f592245
Ivan Kuchin <tadump+git@gmail.com>
2014-05-30 20:26:35 +0200

    find mate v1 and mate v2 binaries

---
+++ lib/mate/bin.rb
@@ -0,0 +1,21 @@
+require 'shellwords'
+
+module Mate
+  module Bin
+    class << self
+      def v1
+        mate_version(/^mate r\d+/) or abort 'Can\'t find mate binary v1'
+      end
+
+      def v2
+        mate_version(/^mate 2.\d+/) or abort 'Can\'t find mate binary v2'
+      end
+
+    private
+
+      def mate_version(regexp)
+        IO.popen('which -a mate', &:readlines).map(&:strip).find{ |bin| `#{bin.shellescape} -v` =~ regexp }
+      end
+    end
+  end
+end
